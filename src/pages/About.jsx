35a9d64b88be0de2d9b017884404061858b26bae
Jacques <jacques.scoffoni@gmail.com>
2023-09-17 22:30:41 -0400

    Barebone file content

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <section>
+      <h1>About Me</h1>
+      <p>Your bio and photo here</p>
+    </section>
+  );
+}
+
+export default AboutMe;
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Project from "./Project";
+
+function Portfolio() {
+  return (
+    <section>
+      <h1>Portfolio</h1>
+
+    </section>
+  );
+}
+
+export default Portfolio;
